40b935ce64be2012143bb944bb6f6da61110b452
MAINAK007-YO <77545084+MAINAK007-YO@users.noreply.github.com>
2021-04-09 21:22:28 +0530

    Add files via upload

---
+++ eyes.py
@@ -0,0 +1,10 @@
+from gtts import gTTS
+from playsound import playsound
+def play_audio(play_audio):
+    playsound(play_audio)
+def convert_to_audio(text):
+    my_audio=gTTS(text)
+    my_audio.save('hello.mp3')
+    play_audio('hello.mp3')
+
+convert_to_audio("hey mainak now this is the time to take a eye exercise break, hurry up mannnnn!!!!!! hurry up!!!!")
